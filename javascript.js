46e2eb65943d529c04daf2e2e73586979d91198c
Arif <153439486+clorifdai@users.noreply.github.com>
2026-03-01 16:15:06 +0800

    Fix change color flex item

--- javascript.js
+++ javascript.js
@@ -1,54 +1,51 @@
 // index.html
 // Project: Etch-a-Sketch
-function addElement() {
-    const body = document.querySelector("body");
-    const containerDiv = document.createElement("div");
-    containerDiv.className = "container";
-    body.appendChild(containerDiv);
 
-    const div = document.querySelector(".container");
-    const subDiv = document.createElement("div");
-    subDiv.className = "sub-container";
-    div.appendChild(subDiv);
+const body = document.querySelector("body");
+const containerDiv = document.createElement("div");
+containerDiv.className = "container";
+body.appendChild(containerDiv);
 
-    const subContainer = document.querySelector(".sub-container");
-    const screenDiv = document.createElement("div");
-    screenDiv.className = "screen-sketch";
-    subContainer.appendChild(screenDiv);
-    
-}
+const div = document.querySelector(".container");
+const subDiv = document.createElement("div");
+subDiv.className = "sub-container";
+div.appendChild(subDiv);
 
-function makeGrids(size) {
-    const screenSketxh = document.querySelector(".screen-sketch");
-    for (let i = 0; i < size; i++ ) {
-        const column = document.createElement("div");
-        column.className = "column";
-        // screenSketxh.appendChild(column);
-        for (let j = 1; j < size; j++) {
-            const row = document.createElement("div");
-            row.className = "row";
-            column.appendChild(row);
-        }
-        screenSketxh.appendChild(column);
+const subContainer = document.querySelector(".sub-container");
+const screenDiv = document.createElement("div");
+screenDiv.className = "screen-sketch";
+subContainer.appendChild(screenDiv);
+
+let size = 20;
+
+const screenSketxh = document.querySelector(".screen-sketch");
+for (let i = 0; i < size; i++ ) {
+    const column = document.createElement("div");
+    column.className = "column";
+    // screenSketxh.appendChild(column);
+    for (let j = 1; j < size; j++) {
+        const row = document.createElement("div");
+        row.className = "row";
+        column.appendChild(row);
     }
+    screenSketxh.appendChild(column);
 }
 
-function makeHover() {
-    const element = document.querySelectorAll(".row");
-    for (let i = 0; i < element.length; i++) {
-        element.addEventListener("click", event => {
-            element[i].style.backgroundColor = "blue";
-        });
+let randomNum = () => {
+    return Math.floor(Math.random() * 256);
+};
 
-    }
+const items = document.querySelectorAll(".row");
 
-}
+items.forEach((item) => {        
+    item.addEventListener("click", () => {
+        let randomColor = `rgb(${randomNum()}, ${randomNum()}, ${randomNum()})`;
+        item.style.backgroundColor = randomColor;
+        console.log("Item diklik! Warna berubah menjadi:", randomColor);
+    });
+});
 
 
-addElement()
-// styleDiv()
-makeGrids(70);
-makeHover()
 
 
 
